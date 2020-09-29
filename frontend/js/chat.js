ef043d8e00fe08437e9d719e5aa508c4c9ca3651
«MaxRubshik» <rubshikmax@gmail.com>
2020-09-29 17:38:49 +0300

    updates

--- frontend/js/chat.js
+++ frontend/js/chat.js
@@ -1,25 +1,18 @@
-
 const url = 'http://localhost:8080';
 let stompClient;
 let selectedUser;
-let newMessages = new Map();
 
 function connectToChat(userName) {
     console.log("connecting to chat...")
     let socket = new SockJS(url + '/chat');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
-        console.log("connected to: " + frame);
+        console.log("connected to: " + frame)
         stompClient.subscribe("/topic/messages/" + userName, function (response) {
             let data = JSON.parse(response.body);
-            if (selectedUser === data.fromLogin) {
-                render(data.message, data.fromLogin);
-            } else {
-                newMessages.set(data.fromLogin, data.message);
-                $('#userNameAppender_' + data.fromLogin).append('<span id="newMessage_' + data.fromLogin + '" style="color: red">+1</span>');
-            }
+            console.log(data);
         });
-    });
+    })
 }
 
 function sendMsg(from, text) {
@@ -38,19 +31,6 @@ function registration() {
             alert("Login is already busy!")
         }
     })
-}
-
-function selectUser(userName) {
-    console.log("selecting users: " + userName);
-    selectedUser = userName;
-    let isNew = document.getElementById("newMessage_" + userName) !== null;
-    if (isNew) {
-        let element = document.getElementById("newMessage_" + userName);
-        element.parentNode.removeChild(element);
-        render(newMessages.get(userName), userName);
-    }
-    $('#selectedUserId').html('');
-    $('#selectedUserId').append('Chat with ' + userName);
 }
 
 function fetchAll() {
